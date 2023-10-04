85d7158d2175dac4bde396ed4c748ee7dac57b20
AliciaCarmonaLopez <146934397+AliciaCarmonaLopez@users.noreply.github.com>
2023-10-04 14:53:14 +0200

    Add files via upload

---
+++ QUERY_ALICIA.c
@@ -0,0 +1,56 @@
+//Incluir esta libreriￂﾭa para poder hacer las llamadas en shiva2.upc.es
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char *argv)
+{
+    MYSQLconn;
+    int err;
+    // Estructura especial para almacenar resultados de consultas 
+    MYSQL_RES *resultado;
+    MYSQL_ROW row;
+
+    //Creamos una conexion al servidor MYSQL 
+    conn = mysql_init(NULL);
+    if (conn==NULL) {
+        printf ("Error al crear la conexi￳n: %u %s\n", 
+                mysql_errno(conn), mysql_error(conn));
+        exit (1);
+    }
+    //inicializar la conexin
+    conn = mysql_real_connect (conn, "localhost","root", "mysql", "Juego",0, NULL, 0);
+    if (conn==NULL) {
+        printf ("Error al inicializar la conexion: %u %s\n", 
+                mysql_errno(conn), mysql_error(conn));
+        exit (1);
+    }
+
+
+    char consulta [80];
+    strcpy (consulta,"SELECT Partidas.Dificultad FROM Partidas,Relaciones,Jugadores WHERE Relaciones.Data = 'Martes'AND Jugdores.Name ='Alicia' AND Jugadores.Name ='Juan' AND Jugadores.ID= Relaciones.ID_J  ");
+
+
+    err=mysql_query (conn, consulta);
+    if (err!=0) {
+        printf ("Error al consultar datos de la base %u %s\n",
+                mysql_errno(conn), mysql_error(conn));
+        exit (1);
+    }
+
+    row = mysql_fetch_row (resultado);
+
+    if (row == NULL)
+        printf ("No se han obtenido datos en la consulta\n");
+    else
+        while (row !=NULL) {
+            // la columna 0 contiene el nombre del jugador
+            printf ("%s\n", row[0]);
+            // obtenemos la siguiente fila
+            row = mysql_fetch_row (resultado);
+    }
+
+    mysql_close (conn);
+    exit(0);
+}
